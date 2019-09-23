072e7cec861e673940724ccc83594fcce223cc74
digitalbricks <mail@andreherdling.de>
2019-09-23 08:01:27 +0200

    added functions for http response in ajax processor

--- bookmarks-ajax-processor.php
+++ bookmarks-ajax-processor.php
@@ -9,12 +9,32 @@ if(isset($_POST) AND isset($_POST['id']) AND isset($_POST['action'])){
     if($_POST['action']=='add'){
         // add to bookmarks
         $bookmarks->add($id);
+        send_response("add",200,"item {$id} added");
     }
 
     if($_POST['action']=='remove'){
         // remove from bookmarks
         $bookmarks->remove($id);
+        send_response("add",200,"item {$id} removed");
     }
+} else {
+    send_response("none",400,"bad request");
 }
 
 
+
+/**
+ * Sends the response, including HTTP status code
+ * @param string $action performed action (for use in JS)
+ * @param int $http_status http status code
+ * @param string $message the message
+ * @return void
+ */
+function send_response($action="",int $http_status=200,$message=""){
+    http_response_code($http_status);
+    $response =  array(
+        "action" => $action,
+        "message" => $message
+    );
+    echo json_encode($response);
+}
